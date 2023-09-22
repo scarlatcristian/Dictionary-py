291dd09d1da72cab28bb0ce818e49bdb2d3ba8dd
cristi <scarlat.cristian.gabriel@gmail.com>
2023-09-22 23:31:29 +0300

    check for mispelled words

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -7,9 +8,13 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return f"Did you mean {get_close_matches(word, data.keys())[0]} instead"
+
     else:
         return f"The word {word} does not exist. Please double check it"
 
 
 word = input("Enter a word: ")
+
 print(translate(word))
